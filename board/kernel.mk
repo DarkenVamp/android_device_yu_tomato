10e06070f4197b4d69c4247a583216931717ceec
DarkenVamp <hiteshd1207@gmail.com>
2018-04-04 10:50:47 +0530

    Change kernel defconfig name

--- board/kernel.mk
+++ board/kernel.mk
@@ -3,5 +3,5 @@ BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
 LZMA_RAMDISK_TARGETS := boot,recovery
-TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
+TARGET_KERNEL_CONFIG := tomato_defconfig
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
